c6e8a90b1803ceb5b3c2e838725e731262ad415b
sajal_2003 <221210093@nitdelhi.ac.in>
2025-05-19 23:40:58 +0530

    add get route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
